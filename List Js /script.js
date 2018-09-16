18f8dbb6d3d5aa23ef106ecde80d6996212cbe0b
Tomas <tomasppp1@gmail.com>
2018-09-16 16:53:35 +0300

    commit

--- List Js /script.js
+++ List Js /script.js
@@ -2,10 +2,25 @@ function addButton(){
     let listUl= document.getElementById("list");
     let input=document.getElementById("input").value;
     let newItem=document.createElement("li")
-    newItem.innerHTML=input;
-    listUl.appendChild(newItem);
-    document.getElementById("input").value="";
+    
     if (input===""){    
         alert("Please fill em")
+    }else{ 
+        newItem.innerHTML=input;
+        listUl.appendChild(newItem);
+        document.getElementById("input").value="";
+    }
+}
+function addButtonSecond(){
+    let listUl= document.getElementById("listSecond");
+    let input=document.getElementById("inputSecond").value;
+    let newItem=document.createElement("li")
+    
+    if (input===""){    
+        alert("Please fill em")
+    }else{ 
+        newItem.innerHTML=input;
+        listUl.appendChild(newItem);
+        document.getElementById("inputSecond").value="";
     }
 }
